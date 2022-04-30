a18ded7507f8fa6838a2ecc68fb28f5971da6619
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-04-30 15:27:19 -0400

    user controllers started

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,23 @@
-const {User,Thought} = require("../models");
+const { User, Thought } = require("../models");
 const userControllers = {
-    createUser(req,res){
-        User.create(req.body).then( (userinfo)=>{
-           res.status(200).json(userinfo)
-        }).catch( (err)=>{
-            res.status(500).json(err)
-        })
-    },
-    //getallUsers();
-}
+  createUser(req, res) {
+    User.create(req.body)
+      .then((userinfo) => {
+        res.status(200).json(userinfo);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+  getallUsers(req, res) {
+    User.find().then((userinfo) => res.json(userinfo));
+  },
 
-module.exports = userControllers; 
 
-// TO DO: create routes and update controllers
+  
+  onlyOneUser(req, res) {},
+  updateUser(req, res) {},
+  userDeleted(req, res) {},
+};
+
+module.exports = userControllers;
